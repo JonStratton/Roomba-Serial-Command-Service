80a2ee17d49840b4268c1e2eff05bf10b1b55dbe
Jon Stratton <jgstratt@sdf.org>
2019-01-12 17:53:06 -0600

    cleaned up zeroconf a little

--- RoombaSerialCommandService.py
+++ RoombaSerialCommandService.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 __author__ = 'Jon Stratton'
-import socket, serial
+import socket, serial, sys, signal
 from zeroconf import ServiceInfo, Zeroconf
 from flask import Flask  
 
@@ -25,11 +25,20 @@ service_host = fqdn.split('.')[0]
 
 # Zeroconf Service settings
 service_zc_desc = {'service': service_name, 'version': '0.1'}
-service_zc_info = ServiceInfo('_http._tcp.local.',
-                   service_host + ' ' + service_name + '._http._tcp.local.',
-                   socket.inet_aton(service_addr), service_port, 0, 0, service_zc_desc)
+service_zc_info = ServiceInfo('_roomba._tcp.local.',
+                   service_host + ':' + str(service_port) + '._roomba._tcp.local.',
+                   service_host, service_port, 0, 0, service_zc_desc)
 zeroconf = Zeroconf()
 
+# cleanup on sigint
+def signal_handler( sig, frame ):
+   # Unregister service with zeroconf
+   zeroconf.unregister_service(service_zc_info)
+   zeroconf.close()
+   sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+# Just write some digits to the serial port
 def roomba_do( command_list ):
    success = 0
    try:
@@ -43,7 +52,6 @@ def roomba_do( command_list ):
    return success
 
 app = Flask(__name__) 
-
 @app.route("/do/<string:command>/")
 def do(command):
     ret_str = 'eh?'
